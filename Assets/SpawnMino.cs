4d3506d28eff90f7c5f733baa8888a1f83aeaa77
R-T <csri16001@gmail.com>
2022-12-19 21:13:12 +0900

    * コード整理

--- Assets/SpawnMino.cs
+++ Assets/SpawnMino.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Networking;
+using System.Linq;
 
 public class SpawnMino : MonoBehaviour
 {
     public GameObject[] Minos;
+    int[] delline1 = new int[] { 1, 1, 1, 1, 1, 1};
+    int[] delline2 = new int[9] { 0, 1, 1, 1, 1, 1, 1, 0, 0 };
     // Start is called before the first frame update
     void Start()
     {
-        NewMino();
+        //bool a = delline2.Contains(delline1);
+        //Debug.Log(a);
+        //await SendWebRequest();
+        // 通信完了を待ってから以下の処理が呼ばれる
+        //Debug.Log("Do something");
     }
 
-    public void NewMino()
-    {
-        Instantiate(Minos[Random.Range(0, Minos.Length)], transform.position, Quaternion.identity);
-    }
+    //public void NewMino()
+    //{
+    //    Instantiate(Minos[Random.Range(0, Minos.Length)], transform.position, Quaternion.identity);
+    //}
+    //async UniTask SendWebRequest()
+    //{
+    //    var request = UnityWebRequest.Get("https://www.google.com/");
+    //    await request.SendWebRequest();
+        // 以下は通信が終わった後に処理される
+    //}
 }
